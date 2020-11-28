4876c3ed1e268660061da8f7342d85c70b642fe0
rpemmire <raghav_pemmireddy@brown.edu>
2020-11-27 22:03:14 -0500

    Everything in reachability except random walks done

--- Code/Model/preprocess.py
+++ Code/Model/preprocess.py
@@ -3,6 +3,7 @@ import numpy as np
 from functools import reduce
 import lzma
 import networkx as nx
+import random
 
 #hyperparameters
 decay_rate = 1/2 #1/(2^delta_t)
@@ -59,8 +60,6 @@ def get_reachabilities(file_path, opt_out, decay_rate):
                 receiver = createEdge[1]
             newEdgeList.append(tuple([sender, receiver]))
 
-
-
         #1) decrement all edge weights by half
         for edge in G.edges(data = True):
             #print(edge[2]['weight'])
@@ -76,7 +75,7 @@ def get_reachabilities(file_path, opt_out, decay_rate):
             src = createEdge[0]
             dst = createEdge[1]
 
-            # if edge already exists, make its attribute 1
+            # if edge already exists, add 1 to attribute
             if G.has_edge(src, dst):
                 G[src][dst]["weight"] +=1
             # else add in new edge with attribute 1
@@ -87,10 +86,11 @@ def get_reachabilities(file_path, opt_out, decay_rate):
         to_remove = []
         for node in G.nodes():
             edges = G.edges(node, data = True)
-            sum = 0
+            max = 0
             for edge in edges:
-                sum += edge[2]['weight']
-            if sum <= opt_out:
+                if edge[2]['weight'] > max:
+                    max = edge[2]['weight']
+            if max <= opt_out:
                 to_remove.append(node)
         G.remove_nodes_from(to_remove)
         print(len(to_remove), "nodes removed")
@@ -119,19 +119,97 @@ def get_amounts(file_path):
 def get_randomWalks(G, opt_out, length, walks_per_node):
     #get random walks for a given graph
     #output all sequences in forms of np.array(walks by walk_length)
+    #10 walks each node at each timestep, training on all previousy created, but recently updated, walks
 
     #for each node, get 10 random walks of length 40
-        sequences = []
-        #while len(sequences != 10)
-
-
+    sequences = []
+    #while len(sequences != 10)
+    # i = 0
+    # while true:
+        #proudce 100 walks
+        #node = nodes[i]
+        #for walkj in walks:
+            #if node in walkj
+            #add walkj to walks of node
+            #if node has 10 walks
+                #i +=1
+                #break
+        # if i = final node index +1
+            #False
 
     pass
 
-def Node2Vec_getData():
-    #return skipgram labels and outputs, with a dictionary, (all translated from 1 to n)
-    pass
+def Node2Vec_getData(randomWalks, numNodes, window_sz):
+    #return skipgram (labels, outputs) tuple np, with a dictionary, (all translated from 1 to n)
 
-def Prediction_getData():
-    #return pairs of nodes, their embeddings, and ground truths
-    pass
+
+    #convert all sequences to ints from 0 to N-1
+    #use tf.keras.preprocessing.sequence.skipgrams after
+    vocab = set(np.array(randomWalks).flatten())
+    indices = list(range(0, len(vocab)))
+    #zip up words and indices
+    vocabdict = dict(zip(list(vocab), indices))
+
+    print(len(vocabdict))
+
+    pairs =[]
+    for walk in randomWalks:
+        walk_ids = [vocabdict[i] for i in walk]
+        pairs.append(tf.keras.preprocessing.sequence.skipgrams(walk_ids, numNodes, window_size = window_sz, negative_samples=0.0))
+    data = np.reshape(np.array(pairs), (-1, 2))
+
+    return data, vocabdict
+
+def Prediction_getData(embeddings, idtoNodeid, G):
+    #return pairs of embeddings(concatenated), and ground truths
+
+    #get numWords
+    num_words = embeddings.shape[0]
+    indices = list(range(num_words))
+
+    #get all possible pairs of words
+    pairs = []
+    for i in indices:
+        for j in indices:
+            pairs.append(zip(i, j))
+
+
+    #translate all to embeddings
+    inputs = []
+    outputs = []
+    for pair in pairs:
+        src = idtoNodeid[pair[0]]
+        dst = idtoNodeid[pair[1]]
+        pair[0] = tf.nn.embedding_lookup(embeddings, pair[0], max_norm=None, name=None)
+        pair[1] = tf.nn.embedding_lookup(embeddings, pair[1], max_norm=None, name=None)
+
+
+        #turn those pairs of embeddings into inputs (concatenate each pair)
+        inputs.append(tf.concat([pair[0], pair[1]], axis=-1))
+        #check if those embeddings (in nodeID form) have connections in the graph
+        if G.has_edge(src, dst) and G[src][dst]["weight"] >= 1:
+            outputs.append(1)
+        else:
+            outputs.append(0)
+
+
+    #negative sampling of outputs of 0 until outputs are equal
+    indices = []
+    for i in range(0, len(outputs)) :
+        if outputs[i] == 0:
+            indices.append(i)
+    indices = random.shuffle(indices)
+
+    #get number of outputs with 1
+    num_out1 = outputs.count(1)
+    num_out0 = len(outputs) - num_out1
+    diff = num_out0 - num_out1
+
+    for i in range(len(diff)):
+        #delete output and input at indices[i]
+        outputs.pop(i)
+        inputs.pop(i)
+
+
+
+    return inputs, outputs
--- Code/Model/reachability.py
+++ Code/Model/reachability.py
@@ -15,13 +15,24 @@ class Node2Vec(tf.keras.Model):
         '''
         Initialize embedding matrix, linear layer, optimizer, sizes, etc.
         '''
+        self.vocab_sz = vocab_size
+        self.embedding_sz = 128
+        self.window_size = 2
+
+        self.learning_rate = 0.001
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        self.epochs = 100
 
 
-    def call(self, inputs, initial_state):
+
+    def call(self, inputs):
         """
         Basic embedding to linear layer
         """
         #TODO: Fill in
+        self.E = tf.Variable(tf.random.truncated_normal([self.vocab_sz,self.embedding_sz], stddev=.1))
+        self.W = tf.Variable(tf.random.truncated_normal([self.embedding_sz, self.vocab_sz], stddev=.1))
+        self.b = tf.Variable(tf.random.truncated_normal([self.vocab_sz], stddev=.1))
 
 
         #embedding layer output has to be (batch_size, window_size, embedding_size)
@@ -30,8 +41,10 @@ class Node2Vec(tf.keras.Model):
         Keras embedding layer to linear output layer
 
         '''
-
-        return probs, final_state
+        embedding = tf.nn.embedding_lookup(self.E,inputs) #output of embedding is batch_size * embedding_size
+        #embedding*W is batch_size*vocab_size
+        logits = tf.matmul(embedding,self.W) + self.b
+        return logits
 
     def loss(self, probs, labels):
         """
@@ -41,7 +54,7 @@ class Node2Vec(tf.keras.Model):
         #TODO: Fill in
         #We recommend using tf.keras.losses.sparse_categorical_crossentropy
         #https://www.tensorflow.org/api_docs/python/tf/keras/losses/sparse_categorical_crossentropy
-        losses = tf.keras.losses.sparse_categorical_crossentropy(labels, probs)
+        losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
         return tf.reduce_mean(losses)
 
 
@@ -54,41 +67,59 @@ class Predictor(tf.keras.Model):
         """
 
         super(Model, self).__init__()
+        self.hidden_layer_sz = 256 #might need to change
+        self.num_classes = 2
+        self.layer1 = tf.keras.layers.Dense(self.hidden_layer_size, activation='relu', name='pred_layer_1')
+        self.layer2 = tf.keras.layers.Dense(self.hidden_layer_size, activation='relu', name='pred_layer_1')
+        self.layer3 = tf.keras.layers.Dense(self.num_classes, activation='softmax', name='pred_layer_1')
 
 
-
-    def call(self, inputs, initial_state):
+    def call(self, inputs):
         """
         feedforward prediction
         """
         #TODO: Fill in
+        output1 = self.layer1(inputs)
+        output2 = self.layer2(output1)
+        probs = self.layer3(output2)
 
 
-        #embedding layer output has to be (batch_size, window_size, embedding_size)
-        #embeddings = []
-
-
-        return probs, final_state
+        return probs
 
     def loss(self, probs, labels):
         """
         Calculates average cross entropy sequence to sequence loss of the prediction
 
-        NOTE: You have to use np.reduce_mean and not np.reduce_sum when calculating your loss
-
-        :param logits: a matrix of shape (batch_size, window_size, vocab_size) as a tensor
-        :param labels: matrix of shape (batch_size, window_size) containing the labels
-        :return: the loss of the model as a tensor of size 1
         """
 
         #TODO: Fill in
         #We recommend using tf.keras.losses.sparse_categorical_crossentropy
         #https://www.tensorflow.org/api_docs/python/tf/keras/losses/sparse_categorical_crossentropy
+        #paper has super complicated way of explaining this but we think it's the same thing
         losses = tf.keras.losses.sparse_categorical_crossentropy(labels, probs)
         return tf.reduce_mean(losses)
 
+    def accuracy(probs, labels):
+        """
+        Calculates averages
 
-def train_Node2Vec(model, train_inputs, train_labels):
+        """
+        predictions = np.equal(np.argmax(probs, axis=1), labels)
+        accuracy = np.sum(predictions)/len(labels)
+        return accuracy
+
+    def f1(probs, labels):
+        """
+        Calculates f1
+
+        """
+        predictions = np.argmax(probs, axis=1)
+        numerator = 2*(tf.compat.v1.metrics.recall(labels, predictions) * tf.compat.v1.metrics.precision(labels, predictions))
+        denom = tf.compat.v1.metrics.recall(labels, predictions) + tf.compat.v1.metrics.precision(labels, predictions)
+        return  numerator/denominator
+
+
+def train_Node2Vec(model, data):
     """
     Runs through one epoch - all training examples.
 
@@ -97,64 +128,99 @@ def train_Node2Vec(model, train_inputs, train_labels):
     :param train_labels: train labels (all labels for training) of shape (num_labels,)
     :return: None
     """
+    for ep in range(model.epochs):
+        curr_loss = 0
+        step = 0
+        for start, end in zip(range(0, len(data) - model.batch_size, model.batch_size), range(model.batch_size, len(data), model.batch_size)):
+            batch_X = data[start:end, 0]
+            batch_Y = data[start:end, 1]
+            with tf.GradientTape() as tape:
+              probs = model.call(batch_X)
+              loss = model.loss(probs, batch_Y)
+            curr_loss += loss
+            step += 1
+            gradients = tape.gradient(loss, model.trainable_variables)
+            model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
+        if ep % 10 == 0:
+            print('Epoch %d\tLoss: %.3f' % (ep, curr_loss / step))
 
     pass
 
 def train_Predict(model, train_inputs, train_labels):
     """
-    Runs through one epoch - all training examples.
 
-    :param model: the initilized model to use for forward and backward pass
-    :param train_inputs: train inputs (all inputs for training) of shape (num_inputs,)
-    :param train_labels: train labels (all labels for training) of shape (num_labels,)
-    :return: None
     """
 
+    loss_list = []
+    #iterating through dataset with batch size
+    for start, end in zip(range(0, len(train_inputs) - model.batch_size, model.batch_size), range(model.batch_size, len(test_inputs), model.batch_size)):
+        batch_X = data[start:end, :]
+        batch_Y = data[start:end, :]
+        #updating gradients
+        with tf.GradientTape() as tape:
+                probs = model.call(batch_X, False)
+                loss = model.loss(probs, batch_Y)
+                model.loss_list.append(loss)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return model.loss_list
 
-    pass
 
 def test_Predict(model, test_inputs, test_labels):
     """
-    hello
+    returns average accuracy, average f1 score
     """
-    return perplexity
+    acc_list = []
+    f1_list = []
+    #iterating through dataset with batch size
+    for start, end in zip(range(0, len(train_inputs) - model.batch_size, model.batch_size), range(model.batch_size, len(test_inputs), model.batch_size)):
+        batch_X = data[start:end, :]
+        batch_Y = data[start:end, :]
+        #updating gradients
+        probs = model.call(batch_X, False)
+
+        acc_list.append(model.accuracy(probs, labels))
+        f1_list.append(model.f1(probs, labels))
+    return avg(acc_list), avg(f1_list)
 
 
 def main():
-
+    prev_walk = None
     #get all 10 graphs and their random sequences
     data_path = '../../data/first100k.dat.xz'
     graphs = get_reachabilities(data_path, .125, .5)
 
+    predict_model = Predictor()
     #initialize prediction (must be trained across graphs)
     for i in range(len(graphs)-1):
         #initialize word2vec (must be reinitialized for each graph)
-
+        numNodes = len(graphs[i].nodes())
+        embed_model = Node2Vec(numNodes)
 
         #get random walks and return as inputs and labels (skipgram)
-        random_walks = get_randomWalks(graphs[i], .125, .5, 40, 10)
-        inputs, labels, nodetoID_dict = Node2Vec_getData(random_walks)
+        random_walks = get_randomWalks(graphs[i], random_walks, .125, .5, 40, 10)
 
-        #train word2vec model to get embeddings (return in a dictionary)
-        nodeIDtoEmbedding = train_Node2Vec(inputs, labels, nodetoID_dict)
+        '''
+        data, nodetoID_dict = Node2Vec_getData(random_walks, numNodes, embed_model.window_size)
 
+        #train word2vec model to get embeddings
+        train_Node2Vec(embed_model, data)
+        embeddings = model.E.read_value()
+        id2Node_dict = {i: w for w, i in nodetoID.items()}
 
         #train a model on the graph to identify the correct edge weight
             #inputs: (node combos, their respective embedding combos)
             #labels: (whether or not those tranactions happen (if edge weight>1))
-        train_inputs, train_labels = Prediction_getData(nodeIDtoEmbedding, graphs[i])
+        train_inputs, train_labels = Prediction_getData(embeddings, id2Node_dict , graphs[i])
+        train_Predict(predict_model, train_inputs, train_labels)
+
         #test prediction model on the next graph
-        test_inputs, test_labels = Prediction_getData(nodeIDtoEmbedding, graphs[i+1])
+        test_inputs, test_labels = Prediction_getData(embeddings, id2Node_dict, graphs[i+1])
+        acc, f1 = test_Predict(predict_model, test_inputs, test_labels)
 
-
-
-
-
-
-
-
-
+        print("reachabilities", i+1, acc, f1)
+        '''
 
     pass
 
